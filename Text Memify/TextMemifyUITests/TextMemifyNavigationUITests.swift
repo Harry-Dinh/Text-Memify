0f8ed2df564dfbf84098d0bb206de890990e40c6
Harry Dinh <harrydinh1207@gmail.com>
2025-12-27 17:42:38 -0500

    Added and configured test target

---
+++ Text Memify/TextMemifyUITests/TextMemifyNavigationUITests.swift
@@ -0,0 +1,12 @@
+//
+//  TextMemifyNavigationUITests.swift
+//  Text Memify
+//
+//  Created by Harry Dinh on 2025-12-27.
+//
+
+import XCTest
+
+class TextMemifyNavigationUITests: TextMemifyUITests {
+    
+}
---
+++ Text Memify/TextMemifyUITests/TextMemifyUITests.swift
@@ -0,0 +1,20 @@
+//
+//  TextMemifyUITests.swift
+//  TextMemifyUITests
+//
+//  Created by Harry Dinh on 2025-12-27.
+//
+
+import XCTest
+
+class TextMemifyUITests: XCTestCase {
+    let app = XCUIApplication()
+    
+    // MARK: - Helper Functions
+    
+    func launchApp() {
+        app.launch()
+    }
+    
+    // MARK: - Sub-test functions
+}
